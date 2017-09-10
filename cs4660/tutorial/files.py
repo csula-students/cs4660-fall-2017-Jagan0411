a5f42e18784d4c3a8649bf5202981d4c94ae39c7
C.L.Jagannathan <cl.jagan@gmail.com>
2017-09-09 22:08:00 -0700

    Revert "Python Exercise"

--- cs4660/tutorial/files.py
+++ cs4660/tutorial/files.py
@@ -3,43 +3,36 @@
 class SimpleFile(object):
     """SimpleFile tests using file read api to do some simple math"""
     def __init__(self, file_path):
-		f = open(file_path)
         self.numbers = []
         """
         TODO: reads the file by path and parse content into two
         dimension array (numbers)
         """
-		text = f.read()
-		lines = text.split('\n')
-		for line in lines:
-			if len(line) > 0:
-				parts = list(map(int,line.split(' ')))
-				self.numbers.append(parts)
 
     def get_mean(self, line_number):
         """
         get_mean retrieves the mean value of the list by line_number (starts
         with zero)
         """
-        return lists.get_avg(self.numbers[line_number])
+        pass
 
     def get_max(self, line_number):
         """
         get_max retrieves the maximum value of the list by line_number (starts
         with zero)
         """
-        return max(self.numbers[line_number])
+        pass
 
     def get_min(self, line_number):
         """
         get_min retrieves the minimum value of the list by line_number (starts
         with zero)
         """
-        return min(self.numbers[line_number])
+        pass
 
     def get_sum(self, line_number):
         """
         get_sum retrieves the sumation of the list by line_number (starts with
         zero)
         """
-        return lists.get_sum(self.numbers[line_number])
+        pass
