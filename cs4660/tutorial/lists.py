5795ff77ab29939afeba1d9bb8bb94cef4792f64
CLJ <cl.jagan@gmail.com>
2017-10-19 17:29:56 -0700

    commit changes

--- cs4660/tutorial/lists.py
+++ cs4660/tutorial/lists.py
@@ -1,21 +1,25 @@
 """Lists defines simple list related operations"""
 
 def get_first_item(li):
+    return li[0]
     """Return the first item from the list"""
-    pass
+    
 
 def get_last_item(li):
-    """Return the last item from the list"""
-    pass
+    
+    return li[-1]
+    
 
 def get_second_and_third_items(li):
-    """Return second and third item from the list"""
-    pass
+    
+    return li[1:3]
+    
 
 def get_sum(li):
-    """Return the sum of the list items"""
-    pass
+    return sum(li)
+    
 
 def get_avg(li):
-    """Returns the average of the list items"""
-    pass
+    list_sum = get_sum(li)
+    list_len = len(li)
+    return list_sum/list_len
